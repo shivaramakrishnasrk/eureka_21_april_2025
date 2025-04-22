2203a2376b9e6671cb57979a0dcc0c905f844e53
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-04-22 21:01:19 +0530

    day-2 examples and notes

---
+++ javascript/day_02_april_22/addition.js
@@ -0,0 +1,7 @@
+let a = 10;
+let b = 20;
+
+console.log(`Addition of ${a} and ${b} is ${a+b}`); // ES-6 
+console.log('Multiplication of '+ a + ' and '+b+' is '+(a*b)); // string concat
+
+window.alert(`Addition of ${a} and ${b} is ${a+b}`); // this statement only works in browser, not in nodejs
