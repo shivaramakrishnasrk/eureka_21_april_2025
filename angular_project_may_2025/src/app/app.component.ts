a6329fb707b50e6c5e8d8a3810d06954a82d3918
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-05-05 21:15:10 +0530

    day 1 angular

---
+++ angular_project_may_2025/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    NavbarComponent,
+    CategoriesComponent,
+    CarouselComponent,
+    BodyComponent,
+    FooterComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular_project_may_2025';
+}
